51bde58192d3d7b164cb176b22fe9fcf0f2b979f
gudangcoding <dientrimedia@gmail.com>
2023-10-31 17:40:40 +0700

    update api produk

--- app/Http/Controllers/API/ProdukController.php
+++ app/Http/Controllers/API/ProdukController.php
@@ -4,8 +4,51 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Produk;
 
 class ProdukController extends Controller
 {
-    //
+    function index()  {
+        $produk = Produk::leftJoin('kategori', 'kategori.id_kategori', 'produk.id_kategori')
+            ->select('produk.*', 'nama_kategori')
+            ->orderBy('kode_produk', 'asc')
+            ->get();
+        return response()->json([
+            'success'=>true,
+            'data'=>$produk
+        ],201);
+    }
+
+    public function store(Request $request)
+    {
+        $produk = Produk::latest()->first() ?? new Produk();
+        $request['kode_produk'] = 'P'. tambah_nol_didepan((int)$produk->id_produk +1, 6);
+
+        $produk = Produk::create($request->all());
+
+        return response()->json('Data berhasil disimpan', 200);
+    }
+
+    public function show($id)
+    {
+        $produk = Produk::find($id);
+
+        return response()->json($produk);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $produk = Produk::find($id);
+        $produk->update($request->all());
+
+        return response()->json('Data berhasil disimpan', 200);
+    }
+
+    public function destroy($id)
+    {
+        $produk = Produk::find($id);
+        $produk->delete();
+
+        return response(null, 204);
+    }
 }

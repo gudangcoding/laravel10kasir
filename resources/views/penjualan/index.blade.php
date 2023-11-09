a93f57b9cfa1595b572bf82314627c2c0ee40b59
gudangcoding <dientrimedia@gmail.com>
2023-11-09 13:20:51 +0700

    warna status berubah pada tabel

--- resources/views/penjualan/index.blade.php
+++ resources/views/penjualan/index.blade.php
@@ -10,120 +10,175 @@
 @endsection
 
 @section('content')
-<div class="row">
-    <div class="col-lg-12">
-        <div class="box">
-            <div class="box-body table-responsive">
-                <table class="table table-stiped table-bordered table-penjualan">
-                    <thead>
-                        <th width="5%">No</th>
-                        <th>Tanggal</th>
-                        <th>Kode Member</th>
-                        <th>Total Item</th>
-                        <th>Total Harga</th>
-                        <th>Diskon</th>
-                        <th>Total Bayar</th>
-                        <th>Status</th>
-                        <th>Kasir</th>
-                        <th width="15%"><i class="fa fa-cog"></i></th>
-                    </thead>
-                </table>
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="box">
+                <div class="box-body table-responsive">
+                    <table class="table table-stiped table-bordered table-penjualan">
+                        <thead>
+                            <th width="5%">No</th>
+                            <th>Tanggal</th>
+                            <th>Kode Member</th>
+                            <th>Total Item</th>
+                            <th>Total Harga</th>
+                            <th>Diskon</th>
+                            <th>Total Bayar</th>
+                            <th>Status</th>
+                            <th>Kasir</th>
+                            <th width="15%"><i class="fa fa-cog"></i></th>
+                        </thead>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
-@includeIf('penjualan.detail')
-
+    @includeIf('penjualan.detail')
 @endsection
 
 @push('scripts')
-<script>
-    let table, table1;
+    <script>
+        let table, table1;
 
-    $(function () {
-        table = $('.table-penjualan').DataTable({
-            processing: true,
-            autoWidth: false,
-            ajax: {
-                url: '{{ route('penjualan.data') }}',
-            },
-            columns: [
-                {data: 'DT_RowIndex', searchable: false, sortable: false},
-                {data: 'tanggal'},
-                {data: 'kode_member'},
-                {data: 'total_item'},
-                {data: 'total_harga'},
-                {data: 'diskon'},
-                {data: 'bayar'},
-                {data: 'status'},
-                {data: 'kasir'},
-                {data: 'aksi', searchable: false, sortable: false},
-            ]
+        $(function() {
+            table = $('.table-penjualan').DataTable({
+                processing: true,
+                autoWidth: false,
+                ajax: {
+                    url: '{{ route('penjualan.data') }}',
+                },
+                columns: [{
+                        data: 'DT_RowIndex',
+                        searchable: false,
+                        sortable: false
+                    },
+                    {
+                        data: 'tanggal'
+                    },
+                    {
+                        data: 'kode_member'
+                    },
+                    {
+                        data: 'total_item'
+                    },
+                    {
+                        data: 'total_harga'
+                    },
+                    {
+                        data: 'diskon'
+                    },
+                    {
+                        data: 'bayar'
+                    },
+                    {
+                        data: 'status'
+                    },
+                    {
+                        data: 'kasir'
+                    },
+                    {
+                        data: 'aksi',
+                        searchable: false,
+                        sortable: false
+                    },
+                ],
+                'rowCallback': function(row, data, index) {
+
+                    if (data.status == 'Selesai') {
+                        $(row).find('td:eq(8)').css('background', 'green');
+
+                    }
+                    if(data.status == 'Pending') {
+                        $(row).find('td:eq(8)').css('background', 'Red');
+
+                    }
+                    if(data.status == 'Dibayar') {
+                        $(row).find('td:eq(8)').css('background', 'orange');
+
+                    }
+                    if(data.status == 'Dikirim') {
+                        $(row).find('td:eq(8)').css('background', 'gray');
+
+                    } 
+
+                },
+            });
+
+            table1 = $('.table-detail').DataTable({
+                processing: true,
+                bSort: false,
+                dom: 'Brt',
+                columns: [{
+                        data: 'DT_RowIndex',
+                        searchable: false,
+                        sortable: false
+                    },
+                    {
+                        data: 'kode_produk'
+                    },
+                    {
+                        data: 'nama_produk'
+                    },
+                    {
+                        data: 'harga_jual'
+                    },
+                    {
+                        data: 'jumlah'
+                    },
+                    {
+                        data: 'subtotal'
+                    },
+                ]
+            })
         });
 
-        table1 = $('.table-detail').DataTable({
-            processing: true,
-            bSort: false,
-            dom: 'Brt',
-            columns: [
-                {data: 'DT_RowIndex', searchable: false, sortable: false},
-                {data: 'kode_produk'},
-                {data: 'nama_produk'},
-                {data: 'harga_jual'},
-                {data: 'jumlah'},
-                {data: 'subtotal'},
-            ]
-        })
-    });
+        function showDetail(url) {
+            $('#modal-detail').modal('show');
 
-    function showDetail(url) {
-        $('#modal-detail').modal('show');
+            table1.ajax.url(url);
+            table1.ajax.reload();
+        }
 
-        table1.ajax.url(url);
-        table1.ajax.reload();
-    }
+        function deleteData(url) {
+            if (confirm('Yakin ingin menghapus data terpilih?')) {
+                $.post(url, {
+                        '_token': $('[name=csrf-token]').attr('content'),
+                        '_method': 'delete'
+                    })
+                    .done((response) => {
+                        table.ajax.reload();
+                    })
+                    .fail((errors) => {
+                        alert('Tidak dapat menghapus data');
+                        return;
+                    });
+            }
+        }
 
-    function deleteData(url) {
-        if (confirm('Yakin ingin menghapus data terpilih?')) {
-            $.post(url, {
-                    '_token': $('[name=csrf-token]').attr('content'),
-                    '_method': 'delete'
-                })
+        function editForm(url, id) {
+            $('#modal-form').modal('show');
+            $('#modal-form .modal-title').text('Edit Produk');
+
+            $('#modal-form form')[0].reset();
+            $('#modal-form form').attr('action', url);
+            $('#modal-form [name=_method]').val('put');
+            $('#modal-form [name=nama_produk]').focus();
+
+            $.get(url)
                 .done((response) => {
-                    table.ajax.reload();
+                    $('#modal-form [name=nama_produk]').val(response.nama_produk);
+                    $('#modal-form [name=id_kategori]').val(response.id_kategori);
+                    $('#modal-form [name=merk]').val(response.merk);
+                    $('#modal-form [name=harga_beli]').val(response.harga_beli);
+                    $('#modal-form [name=harga_jual]').val(response.harga_jual);
+                    $('#modal-form [name=diskon]').val(response.diskon);
+                    $('#modal-form [name=stok]').val(response.stok);
+                    $('#modal-form [name=stok_minimal]').val(response.stok_minimal);
                 })
                 .fail((errors) => {
-                    alert('Tidak dapat menghapus data');
+                    alert('Tidak dapat menampilkan data');
                     return;
                 });
         }
-    }
-
-    function editForm(url,id) {
-        $('#modal-form').modal('show');
-        $('#modal-form .modal-title').text('Edit Produk');
-
-        $('#modal-form form')[0].reset();
-        $('#modal-form form').attr('action', url);
-        $('#modal-form [name=_method]').val('put');
-        $('#modal-form [name=nama_produk]').focus();
-
-        $.get(url)
-            .done((response) => {
-                $('#modal-form [name=nama_produk]').val(response.nama_produk);
-                $('#modal-form [name=id_kategori]').val(response.id_kategori);
-                $('#modal-form [name=merk]').val(response.merk);
-                $('#modal-form [name=harga_beli]').val(response.harga_beli);
-                $('#modal-form [name=harga_jual]').val(response.harga_jual);
-                $('#modal-form [name=diskon]').val(response.diskon);
-                $('#modal-form [name=stok]').val(response.stok);
-                $('#modal-form [name=stok_minimal]').val(response.stok_minimal);
-            })
-            .fail((errors) => {
-                alert('Tidak dapat menampilkan data');
-                return;
-            });
-    }
-</script>
+    </script>
 @endpush

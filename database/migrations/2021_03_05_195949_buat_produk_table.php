b850ba5146559c0055278485668603b484d71816
gudangcoding <dientrimedia@gmail.com>
2023-11-16 15:38:24 +0700

    perbaikan tabel produk kolom satuan

--- database/migrations/2021_03_05_195949_buat_produk_table.php
+++ database/migrations/2021_03_05_195949_buat_produk_table.php
@@ -25,7 +25,7 @@ class BuatProdukTable extends Migration
             $table->integer('stok');
             $table->integer('stok_minimal');
             $table->unsignedBigInteger('id_kategori');
-            $table->integer('id_satuan');
+            $table->string('satuan',30);
             //$table->foreign('id_kategori')->references('id_kategori')->on('kategori')->onDelete('cascade');
             $table->timestamps();
         });
